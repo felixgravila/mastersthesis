2592a841716eece72f96e3b6f600810a885b6ecb
Felix Gravila <felixgravila@gmail.com>
2020-03-03 19:11:12 +0100

    Add maxpool functionality and improve model naming

--- src/models/ChironBuilder.py
+++ src/models/ChironBuilder.py
@@ -5,18 +5,29 @@ class ChironBuilder():
     def __init__(self, input_length):
         self.input_length = input_length
         self.rnn_padding = 0
+        self.maxpool_layer = 0
         self.batch_normalization = False
+        self.model_name = "chiron"
 
     def with_batch_normalization(self):
         self.batch_normalization = True
+        self.model_name += "-bn"
         return self
 
     def with_rnn_padding(self, rnn_padding):
         self.rnn_padding = rnn_padding
-        return self         
+        self.model_name += f"-pad{rnn_padding}"
+        return self
+
+    def with_maxpool(self, maxpool_layer):
+        self.maxpool_layer = maxpool_layer
+        self.model_name += f"-maxpool{maxpool_layer}"
+        return self
     
     def build(self):
         return Chiron(
             input_length=self.input_length,
             rnn_padding = self.rnn_padding,
-            batch_normalization = self.batch_normalization)
+            batch_normalization = self.batch_normalization,
+            maxpool_layer=self.maxpool_layer,
+            model_name=self.model_name)
--- src/train-chiron-demo.py
+++ src/train-chiron-demo.py
@@ -1,5 +1,3 @@
-# %%
-
 import sys
 import math
 
@@ -28,21 +26,18 @@ generator = DataGenerator(read_ids, batch_size=10000, input_length=input_length,
 val_read_ids = data_preper.get_validation_read_ids()
 val_generator = DataGenerator(val_read_ids, batch_size=500, input_length=input_length, stride=150, reads_count=5, rnn_pad_size=rnn_padding)
 
-#%%
-
 chiron = ChironBuilder(input_length)\
             .with_batch_normalization()\
             .with_rnn_padding(rnn_padding)\
+            .with_maxpool(3)\
             .build()
 
 save_cb = SaveCB(chiron, val_generator)\
     .withCheckpoints("model_output")\
     .withImageOutput("image_output")
 
-epoch = 1
+epoch = 0
 while True:    
     generator.print_status()
     X,y = next(generator.get_batch())
     chiron.fit(X, y, initial_epoch=epoch, epochs=epoch+1, callbacks=[save_cb])
-
-# %%
