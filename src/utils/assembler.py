dae60877c142b98b2ccb4c5101b9234b38441ae3
Felix Gravila <felixgravila@gmail.com>
2020-03-16 15:24:20 +0100

    Assembler skip aligning sequences shorter than 3

--- src/utils/assembler.py
+++ src/utils/assembler.py
@@ -49,12 +49,18 @@ def _get_assembled_string(alignments):
     zipped = list(zip_longest(*padded, fillvalue=" "))
     return "".join(list(map(_get_most_popular_base, zipped)))
 
+"""
+seq_list : ['AATTAA', 'ATAAC',...] 
+returns seq_list made into a tuple, where index is the index
+"""
 def _get_aligned_sequences(seq_list,window):
     aligned_seq_list = []
     
     for i, seq in enumerate(seq_list):
-        window_seq_list = aligned_seq_list[max(0, i-window):i]
-        index = _find_alignment_index(window_seq_list, seq)
+        window_seq_list = aligned_seq_list[max(0, i-window):i] # this is just the context window
+        if len(seq) < 3:
+          continue
+        index = _find_alignment_index(window_seq_list, seq) # this is what computes the offset of seq
         aligned_seq_list.append((index,seq))
 
     return aligned_seq_list
@@ -66,13 +72,13 @@ def _find_alignment_index(seq_list, seq_to_align):
   max_score = 0
   max_score_index = 0
 
-  alignment_from = _get_closest_index(seq_list)-len(seq_to_align)+1
-  alignment_to = _get_furthest_index(seq_list)-1
-  
+  alignment_from = _get_closest_index(seq_list)-len(seq_to_align)+1 # returns the smallest `index` in the context window
+  alignment_to = _get_furthest_index(seq_list)-1 # returns the furthest away character
+
   for i in range(alignment_from, alignment_to):
     score = _calc_score(seq_list, seq_to_align, i)
     if(score > max_score):
-      max_score, max_score_index = (score, i)
+      max_score, max_score_index = score, i
   return max_score_index
 
 def _calc_score(seq_list, seq_to_align, index):
