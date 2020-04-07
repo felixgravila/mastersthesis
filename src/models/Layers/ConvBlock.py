d632276cfa8d13d9327e17e44e2258d86cc1cedf
Felix Gravila <felixgravila@gmail.com>
2020-04-07 10:40:10 +0200

    :art: Implement structure to train FishNChips without resblocks (#71)
    * :art: Implement structure to train FishNChips without resblocks
    
    Introduce one beginning Conv1D layer to create the desired dimensionality in the case num_cnn_blocks is 0
    
    * Add BN to ConvBlocks

--- src/models/Layers/ConvBlock.py
+++ src/models/Layers/ConvBlock.py
@@ -4,18 +4,15 @@ class ConvolutionBlock(tf.keras.layers.Layer):
     def __init__(self, cnn_dims, filters, idx):
         super(ConvolutionBlock, self).__init__()
 
-        self.residual = tf.keras.layers.Conv1D(filters, 1, padding="same", name=f"res{idx}-r")      
         self.cnn_layers = [tf.keras.layers.Conv1D(filters, dim, padding="same", activation="relu", use_bias="false", name=f"res{idx}-c{i}") for i,dim in enumerate(cnn_dims)]
+        self.bn_layers = [tf.keras.layers.BatchNormalization() for _ in cnn_dims]
         self.activation_layer = tf.keras.layers.Activation('relu', name=f"res{idx}-relu")
         
     def call(self, x):
-        if x.shape[-1] == 1:
-            res = self.residual(x)
-        else:
-            res = x
-        
-        for cnn_layer in self.cnn_layers:
+        res = x
+        for cnn_layer, bn_layer in zip(self.cnn_layers, self.bn_layers):
             x = cnn_layer(x)
+            x = bn_layer(x)
         
         x += res
         x = self.activation_layer(x)
