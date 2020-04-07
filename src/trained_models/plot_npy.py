b91f7706da4b8784e3971205f85e7a3217bcf98c
Felix Gravila <felixgravila@gmail.com>
2020-04-07 12:09:23 +0200

    util to plot npys

---
+++ src/trained_models/plot_npy.py
@@ -0,0 +1,15 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+
+file = "train_res_256_0CNN-8H.npy"
+prefix = "/".join(sys.argv[0].split("/")[:-1])
+if len(sys.argv) > 1:
+    file = sys.argv[1]
+
+data = np.load(f"{prefix}/{file}")
+plt.plot(data[:,0], label="loss")
+plt.plot(data[:,1], label="acc")
+plt.xlabel("batches")
+plt.legend()
+plt.show()
