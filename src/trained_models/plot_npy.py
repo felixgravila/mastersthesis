282c63ed849db01a56d7687b255c595c526fec0a
Felix Gravila <felixgravila@gmail.com>
2020-04-08 14:18:24 +0200

    Fix argv bug in plot_npy

--- src/trained_models/plot_npy.py
+++ src/trained_models/plot_npy.py
@@ -4,10 +4,12 @@ import matplotlib.pyplot as plt
 
 file = "train_res_256_0CNN-8H.npy"
 prefix = "/".join(sys.argv[0].split("/")[:-1])
+if len(prefix)>0:
+    prefix += "/"
 if len(sys.argv) > 1:
     file = sys.argv[1]
 
-data = np.load(f"{prefix}/{file}")
+data = np.load(f"{prefix}{file}")
 plt.plot(data[:,0], label="loss")
 plt.plot(data[:,1], label="acc")
 plt.xlabel("batches")
