BREAKING CHANGE FIX: Fish and Chips - forgotten decoder CNN layer | FNC_CTC_Combined - tried removing combined loss | Train FNC - added AED calculation every 5 epochs

src/models/FishNChips.py
@@ -24,10 +24,10 @@ class FishNChips(tf.keras.Model):
     #@with_timer
     def call(self, inp, tar, training, look_ahead_mask, use_cached_enc_ouput=False):
         x_enc = self.first_encoder_cnn(inp) # to bring to proper dimensionality
-        x_dec = self.first_decoder_cnn(tar) # to bring to proper dimensionality for ctc 8->d_model
+        #x_dec = self.first_decoder_cnn(tar) # to bring to proper dimensionality for ctc 8->d_model
         
         x_enc = self.call_cnn_blocks(x_enc) # won't do anything if no cnn blocks
-        att_output, att_weights = self.transformer(x_enc, x_dec, training, look_ahead_mask, use_cached_enc_ouput)
+        att_output, att_weights = self.transformer(x_enc, tar, training, look_ahead_mask, use_cached_enc_ouput)
         return att_output, att_weights
 
     #@with_timer  

src/train-fish-n-chips-ctsea.py
@@ -78,10 +78,13 @@ def train_step(inp, tar, labels, input_length, label_length):
   with tf.GradientTape() as tape: 
     enc_out, dec_out = fish(inp, tar_inp, True, combined_mask)
     
-    loss_ctc = tf.keras.backend.ctc_batch_cost(labels, enc_out, input_length, label_length)
-    loss_dec = get_decoder_loss(tar_real, dec_out, loss_object)
-    alpha = 0.2
-    loss_ = alpha * loss_ctc + (1-alpha) * loss_dec
+    # loss_ctc = tf.keras.backend.ctc_batch_cost(labels, enc_out, input_length, label_length)
+    # loss_dec = get_decoder_loss(tar_real, dec_out, loss_object)
+    # alpha = 0.2
+    # loss_ = alpha * loss_ctc + (1-alpha) * loss_dec
+    # loss = tf.reduce_mean(loss_)
+
+    loss_ = get_decoder_loss(tar_real, dec_out, loss_object)
     loss = tf.reduce_mean(loss_)
 
   gradients = tape.gradient(loss, fish.trainable_variables)

src/train-fishnchips.py
@@ -2,6 +2,7 @@
 import time
 import tensorflow as tf 
 import numpy as np
+import editdistance
 
 from utils.AttentionDataGenerator import AttentionDataGenerator
 from utils.DataPrepper import DataPrepper
@@ -10,14 +11,15 @@ from utils.Other import set_gpu_growth, print_tensor_to_file
 from models.Attention.CustomSchedule import CustomSchedule
 from models.Attention.attention_utils import create_combined_mask
 from models.FishNChips import FishNChips
+from utils.attention_evaluation_utils import evaluate_batch
 
 set_gpu_growth()
 
 #%%
 EPOCHS = 5000
 PATIENCE = 300
-NO_BATCHES = 200
-BATCH_SIZE = 32
+NO_BATCHES = 10
+BATCH_SIZE = 3
 
 ENCODER_MAX_LENGTH = 300
 DECODER_MAX_LENGTH = 100
@@ -75,6 +77,20 @@ def train_step(inp, tar):
   train_loss(loss)
   train_accuracy(tar_real, predictions)
 
+  
+def test_edit_distance(output=False):
+    print("evaluating...")
+    x_batch, y_batch_true = next(generator.get_batch(label_as_bases=True))
+    y_batch_pred, _ = evaluate_batch(x_batch, fish, BATCH_SIZE, as_bases=True)
+
+    eds = []
+    for _, (t, p) in enumerate(zip(y_batch_true, y_batch_pred)):
+      ed = editdistance.eval(t, p)
+      eds.append(ed)
+      if(output):
+        print(f"ED:{ed}, True:{t}, Pred:{p}")
+    return np.array(eds).mean()
+
 #%%
 old_loss = 1
 accs = []
@@ -102,6 +118,10 @@ for epoch in range(EPOCHS):
     print (f'Epoch {epoch + 1} Loss {loss:.4f} Accuracy {acc:.4f}')
     print (f'Time taken for 1 epoch: {time.time() - start} secs\n')
 
+    if epoch % 5 == 0:
+      aed = test_edit_distance(output=True)
+      print(f"AED: {aed}")
+
     if loss < old_loss:
       waited = 0
       old_loss = loss
@@ -112,3 +132,4 @@ for epoch in range(EPOCHS):
             print("Out of patience, exiting...")
             break
          
+