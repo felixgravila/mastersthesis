b416e813ecb6dee5b412317490e32d4943861f8b
Felix Gravila <felixgravila@gmail.com>
2020-04-15 12:58:49 +0200

    Add shm location for claaudia

--- src/utils/Other.py
+++ src/utils/Other.py
@@ -47,7 +47,8 @@ def set_gpu_growth():
         
 def get_valid_taiyaki_filename():
     possible_filenames = ["/mnt/nvme/taiyaki_aligned/mapped_umi16to9.hdf5",
-			  "/user/student.aau.dk/fgravi18/data/mapped_therest.hdf5",
+                          "/dev/shm/data/umi11to5.hdf5", #ram on claaudia
+			              "/user/student.aau.dk/fgravi18/data/mapped_therest.hdf5",
                           "/ssd/mapped_therest.hdf5",
                           "/Users/felix/MsC/DNA/mapped_umi16to9.hdf5",
                           "c:/Users/mirop/OneDrive/Documents/Programming/Data/bdm/umi11to5.hdf5",
