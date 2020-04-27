be613693fae477746c3ba25a63805dec7f79b841
Felix Gravila <felixgravila@gmail.com>
2020-04-27 12:59:36 +0200

    Plotter improvements

--- src/trained_models/plot_json.py
+++ src/trained_models/plot_json.py
@@ -5,14 +5,16 @@ import numpy
 
 files = [
     "trained_models/fish_eval_1024_0CNN_8H.json",
-    "trained_models/fishnchips_256_0CNN_4H_4B.json",
-    "trained_models/fishnchips_256_0CNN_16H_4B.json",
+    #"trained_models/fishnchips_256_0CNN_4H_4B.json",
+    #"trained_models/fishnchips_256_0CNN_16H_4B.json",
     "trained_models/fishnchips_256_0CNN_8H_2B.json",
     # "trained_models/fishnchips_256_0CNN_8H_8B.json",
-    "trained_models/fishnctsea_512_0CNN_8H_4B.json",
-    "trained_models/fishnchips_256_0CNN_8H_1B.json",
+    #"trained_models/fishnctsea_512_0CNN_8H_4B.json",
+    #"trained_models/fishnchips_256_0CNN_8H_1B.json",
     "trained_models/fishnchips_250_0CNN_25H_4B.json",
-    "trained_models/fishnchips_256_0CNN_32H_4B.json"
+    # "trained_models/fishnchips_256_0CNN_32H_4B.json",
+    "trained_models/fishnchips_1000_0CNN_25H_2B.json",
+    "trained_models/fishnchips_250_5CNN_25H_4B.json"
 ]
 
 plt.figure(figsize=(20, 10))
@@ -25,7 +27,10 @@ for filepath in files:
         avg = numpy.mean(cigaccs)
         cleaned_filename = filepath.split("/")[1].split(".")[0]
 
-        plt.hist(cigaccs, density=True, alpha=0.8, bins=range(0,100,2), label=f"{cleaned_filename}: {avg:.2f}% ({len(cigaccs)})")
+        plt.hist(cigaccs, density=True, alpha=0.8, bins=range(0,101,2), label=f"{cleaned_filename}: {avg:.2f}% ({len(cigaccs)})")
+        plt.xlabel("% accuracy")
+        plt.ylabel("Amount of reads")
+        plt.xticks(range(0,101,5))
 
 plt.legend()
 plt.show()
